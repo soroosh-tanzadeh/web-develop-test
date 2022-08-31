ad71a859cd69d61c6cb2d67ebe03f29baaeec01d
Soroosh Tanzadeh <soroosh@ternobo.com>
2022-08-31 16:22:26 +0430

    init

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function sum() {
+    var a = parseInt(document.querySelector("#first-input").value);
+    var b = parseInt(document.querySelector("#second-input").value);
+    alert(a + b);
+}
+
+function divide() {
+    var a = parseInt(document.querySelector("#first-input").value);
+    var b = parseInt(document.querySelector("#second-input").value);
+    alert(a / b);
+}
+
+function power() {
+    var a = parseInt(document.querySelector("#first-input").value);
+    var b = parseInt(document.querySelector("#second-input").value);
+    alert(a ** b);
+}
+
+function multiply() {
+    var a = parseInt(document.querySelector("#first-input").value);
+    var b = parseInt(document.querySelector("#second-input").value);
+    alert(a * b);
+}
